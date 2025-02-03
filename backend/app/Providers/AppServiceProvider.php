72a59eaff285c9f343b8de0be90ca2eeecbc69ec
Arkanullah Saad <120630818+arkanullahs@users.noreply.github.com>
2025-02-03 14:41:02 +0600

    rebase

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Register any application services, such as custom services you want to provide, like EmailService, UsersService etc.
      *
      * @return void
      */
