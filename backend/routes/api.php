b8d16d6591a541706163119e053a98520a03cdf9
arkanullahs <arkanullah.cse.20220104011@aust.edu>
2025-02-23 03:18:02 +0600

    updated routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\PublisherController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\PurchaseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +15,8 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('jwt.auth')->group(function () {
 
+    Route::get('/books', [BookController::class, 'index']);
+    Route::get('/books/{id}', [BookController::class, 'show']);
     Route::get('/profile', function (Request $request) {
         return response()->json([
             'message' => 'Welcome to your profile',
@@ -30,5 +35,21 @@ Route::middleware('jwt.auth')->group(function () {
 
     Route::middleware('role:user')->group(function () {
         Route::get('/user/dashboard', [UserController::class, 'index']);
+    });
+    Route::middleware(['jwt'])->group(function () {
+
+        // User routes
+        Route::middleware(['role:user'])->group(function () {
+            Route::post('/books/{id}/buy', [PurchaseController::class, 'store']);
+            Route::get('/user/purchases', [PurchaseController::class, 'index']);
+            Route::post('/books/{id}/review', [ReviewController::class, 'store']);
+        });
+    
+        // Publisher routes
+        Route::middleware(['role:publisher'])->group(function () {
+            Route::post('/books', [BookController::class, 'store']);
+            Route::put('/books/{id}', [BookController::class, 'update']);
+            Route::delete('/books/{id}', [BookController::class, 'destroy']);
+        });
     });
 });
