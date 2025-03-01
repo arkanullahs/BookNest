5d6fe7bd30fe6841e316ee6ef293a3261588b9c1
ahnaf217 <ahnaflabib56@gmail.com>
2025-03-01 21:33:01 +0600

    added role option in sign up

--- frontend/src/components/newLoginSignup.jsx
+++ frontend/src/components/newLoginSignup.jsx
@@ -5,73 +5,82 @@ const SignInSignUp = () => {
   const [signUpMode, setSignUpMode] = useState(false);
 
   return (
-    <div class="login-container">
-    <div className={`container ${signUpMode ? "sign-up-mode" : ""}`}>
-      <div className="forms-container">
-        <div className="signin-signup">
-          <form action="#" className="sign-in-form">
-            <h2 className="title">Sign in</h2>
-            <div className="input-field">
-              <i className="fas fa-user"></i>
-              <input type="text" placeholder="Username" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
-            </div>
-            <input type="submit" value="Login" className="btn solid" />
-            
-            
-          </form>
-          <form action="#" className="sign-up-form">
-            <h2 className="title">Sign up</h2>
-            <div className="input-field">
-              <i className="fas fa-user"></i>
-              <input type="text" placeholder="Username" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
-            </div>
-            <input type="submit" className="btn" value="Sign up" />
-            
-          </form>
+    <div className="login-container">
+      <div className={`container ${signUpMode ? "sign-up-mode" : ""}`}>
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form action="#" className="sign-in-form">
+              <h2 className="title">Sign in</h2>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" value="Login" className="btn solid" />
+            </form>
+            <form action="#" className="sign-up-form">
+              <h2 className="title">Sign up</h2>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-envelope"></i>
+                <input type="email" placeholder="Email" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input type="password" placeholder="Confirm Password" />
+              </div>
+              <p className="role-text">Select Role</p>
+              <div className="dropdown-container">
+                <select className="role-dropdown">
+                  <option value="user">User</option>
+                  <option value="admin">Admin</option>
+                  <option value="publisher">Publisher</option>
+                </select>
+              </div>
+              <input type="submit" className="btn" value="Sign up" />
+            </form>
+          </div>
         </div>
-      </div>
 
-      <div className="panels-container">
-        <div className="panel left-panel">
-          <div className="content">
-            <h3>New here ?</h3>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
-              ex ratione. Aliquid!
-            </p>
-            <button className="btn transparent" onClick={() => setSignUpMode(true)}>
-              Sign up
-            </button>
+        <div className="panels-container">
+          <div className="panel left-panel">
+            <div className="content">
+              <h3>New here ?</h3>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+                ex ratione. Aliquid!
+              </p>
+              <button className="btn transparent" onClick={() => setSignUpMode(true)}>
+                Sign up
+              </button>
+            </div>
+            <img src="img/log.svg" className="image" alt="" />
           </div>
-          <img src="img/log.svg" className="image" alt="" />
-        </div>
-        <div className="panel right-panel">
-          <div className="content">
-            <h3>One of us ?</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-              laboriosam ad deleniti.
-            </p>
-            <button className="btn transparent" onClick={() => setSignUpMode(false)}>
-              Sign in
-            </button>
+          <div className="panel right-panel">
+            <div className="content">
+              <h3>One of us ?</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                laboriosam ad deleniti.
+              </p>
+              <button className="btn transparent" onClick={() => setSignUpMode(false)}>
+                Sign in
+              </button>
+            </div>
+            <img src="img/register.svg" className="image" alt="" />
           </div>
-          <img src="img/register.svg" className="image" alt="" />
         </div>
       </div>
-    </div>
     </div>
   );
 };
